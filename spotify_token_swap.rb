0483104c17c3f79545ac9bab441664ef97424699
pbirdp <p_vogels@yahoo.com>
2016-02-21 17:22:59 +0100

    Update spotify_token_swap.rb
    add new login credentials

--- spotify_token_swap.rb
+++ spotify_token_swap.rb
@@ -37,9 +37,9 @@ require 'encrypted_strings'
 #
 # Find your app at https://developer.spotify.com/my-applications/#!/applications
 
-CLIENT_ID = "e6695c6d22214e0f832006889566df9c" # TODO: Get it from Spotify
-CLIENT_SECRET = "29eb02041ba646179a1189dccac112c7" # TODO: Get it from Spotify
-CLIENT_CALLBACK_URL = "spotifyiossdkexample://" # TODO: Get it from Spotify
+CLIENT_ID = "1b2509e4e1914adeb9fec5ced9aff56c" # TODO: Get it from Spotify
+CLIENT_SECRET = "c96f473ef55d4477abc97957fc3fe87b" # TODO: Get it from Spotify
+CLIENT_CALLBACK_URL = "spotinfo2://" # TODO: Get it from Spotify
 
 # I don't think the secret should be changed...
 ENCRYPTION_SECRET = "cFJLyifeUJUBFWdHzVbykfDmPHtLKLGzViHW9aHGmyTLD8hGXC"
